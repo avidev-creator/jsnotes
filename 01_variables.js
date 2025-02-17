3d252f3d477c71f12f6e384f84139bffe0d681e8
Avinash Dubey <avidub371@gmail.com>
2025-02-17 10:03:03 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,32 @@
+//  single line comment
+/*
+    Variables = memory location to store some value
+    Define - 1. Let 2. Const 3. Var - depricated
+*/
+
+// 1. Let - it will allow you to create a variable that can change values
+
+// let firstName = "avinash" //variable name do not contain any space
+
+// console.log(firstName)
+
+// console.log() is a method to display the variable in terminal
+// syntax - console.log(variable name)
+
+// let age = 34
+// console.log(age);
+
+// age = 44
+// console.log(age);
+// this is how you can change the value of a variable
+
+
+// 2. Const - this will allow you to create a variable but you can never change its value
+
+// const age = 30
+// console.log(age);
+
+// age = 40
+// console.log(age);
+// you can not change a value of a constant variable
+
